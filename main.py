413911cf31dac5e3e1404abce33a5a25597bb046
Atef <atef.toumi@tu-dortmund.de>
2022-03-15 23:57:25 +0100

    add block added

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import hashlib
+from utils import hash_file
 
 
 class DreamCoinsBlock:
 
-    def __init__(self, previous_block_hash, transaction_list):
-        self.previous_block_hash = previous_block_hash
-        self.transaction_list = transaction_list
+    def __init__(self, filename, transaction):
+        self.filename = filename
+        self.transaction = transaction
 
-        self.block_data = " - ".join(transaction_list) + " - " + previous_block_hash
-        self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+    def add_block(self):
+        # f = open(self.filename, "w")
+        # f.write(self.transaction)
+        hash_file(self.filename, self.transaction)
+        # sign_file(self.filename)
+
+    # transaction = "Anna sends 3 DC to Mike"
+    # add_block( transaction,"T1",transaction)
 
 
 t1 = "Anna sends 2 DC to Mike"
@@ -18,17 +24,22 @@ t4 = "Daniel sends 0.3 DC to Anna"
 t5 = "Mike sends 1 DC to Charlie"
 t6 = "Mike sends 5.4 DC to Daniel"
 
-initial_block = DreamCoinsBlock("Initial String", [t1, t2])
+initial_block = DreamCoinsBlock("T1", t2)
+initial_block.add_block()
 
-print(initial_block.block_data)
-print(initial_block.block_hash)
-
-second_block = DreamCoinsBlock(initial_block.block_hash, [t3, t4])
-
-print(second_block.block_data)
-print(second_block.block_hash)
-
-third_block = DreamCoinsBlock(second_block.block_hash, [t5, t6])
-
-print(third_block.block_data)
-print(third_block.block_hash)
+# second_block = DreamCoinsBlock("T1",t2)
+#
+# initial_block = DreamCoinsBlock("Initial String", [t1, t2])
+#
+# print(initial_block.block_data)
+# print(initial_block.block_hash)
+#
+# second_block = DreamCoinsBlock(initial_block.block_hash, [t3, t4])
+#
+# print(second_block.block_data)
+# print(second_block.block_hash)
+#
+# third_block = DreamCoinsBlock(second_block.block_hash, [t5, t6])
+#
+# print(third_block.block_data)
+# print(third_block.block_hash)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import gnupg
 from fs import open_fs
 
@@ -5,21 +7,30 @@ gpg = gnupg.GPG(gnupghome='/home/atef/.gnupg')
 home_fs = open_fs(".")
 
 
-# if os.path.exists("Signatures/"):
-#     print("Signatures directory already created")
-# else :
-#     home_fs.makedir(u"Signatures")
-#     print("Created signatures directory")
+# Hashes a file and writes the hashed string in the same file
+def hash_file(filename, transaction):
+    data = open(filename)
+    data_transactions = data.read()
+    print(data_transactions)
+    hashed_transaction = hashlib.sha256(transaction.encode()).hexdigest()
+    print(hashed_transaction)
+    hashed_data = open(filename, "a")
+    hashed_data.write("\n"+"||| Transaction : " + transaction+" ||| Hash : " + hashed_transaction)
+    # print(data.read())
 
-#Signs a file using gpg, creates a ".asc" file and prints the status
-def sign_file(file):
-    stream = open(file, 'rb')
-    signed_data = gpg.sign_file(stream, passphrase="thisisformybachelorarbeit", output=file + ".asc")
+
+
+
+# Signs a file using gpg, creates a ".asc" file and prints the status
+def sign_file(filename):
+    stream = open(filename, 'rb')
+    signed_data = gpg.sign_file(stream, passphrase="thisisformybachelorarbeit", output=filename + ".asc")
     print("status : ", signed_data.status)
 
-#Verifies the signature of the file and prints if the signature is valid or invalid
-def verify_signature(file):
-    stream = open(file, 'rb')
+
+# Verifies the signature of the file and prints if the signature is valid or invalid
+def verify_signature(filename):
+    stream = open(filename, 'rb')
     verified = gpg.verify_file(stream)
     if verified.status == "signature valid":
         print("Signature is valid")
