bc9a6ca5138f46c395d0e5d13c6a01882259fa59
Atef <atef.toumi@tu-dortmund.de>
2022-04-11 12:57:24 +0200

    added access to blockchain api

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+path = "/home/atef/PycharmProjects/BasicBlockchain"
 
 app = FastAPI()
 
@@ -8,6 +13,9 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"{item_id}": item_id}
+@app.get("/blockchain/{block_name}")
+async def show_block(block_name: str):
+    file_path = os.path.join(path, block_name)
+    if os.path.exists(file_path):
+        return FileResponse(file_path)
+    return {"error": "File not found"}
